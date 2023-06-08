8dade153703915b844de35d9b80ddbd527237988
Vladyslav <vladsw764@icloud.com>
2023-06-08 23:25:13 +0300

    Added method 'findAllByCategory'

--- src/main/java/com/example/accounting_system/repositories/DebtRepository.java
+++ src/main/java/com/example/accounting_system/repositories/DebtRepository.java
@@ -3,5 +3,8 @@ package com.example.accounting_system.repositories;
 import com.example.accounting_system.entities.Debt;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DebtRepository extends JpaRepository<Debt, Long> {
+    List<Debt> findAllByCategory(String category);
 }
