c38b22aa12b660740b8c1d8ac7325b7a40d5f0df
Vladyslav <vladsw764@icloud.com>
2023-06-08 23:24:39 +0300

    Added CategoryController

---
+++ src/main/java/com/example/accounting_system/controllers/CategoryController.java
@@ -0,0 +1,28 @@
+package com.example.accounting_system.controllers;
+
+import com.example.accounting_system.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class CategoryController {
+    private final CategoryService categoryService;
+    @GetMapping("/{category}")
+    public ResponseEntity<List<Object>> getAllByCategory(@PathVariable("category") String category) {
+        return ResponseEntity.ok(categoryService.getTransactionsAndDebtsByCategory(category));
+    }
+
+    @GetMapping("/balance")
+    public ResponseEntity<BigDecimal> getCurrentBalance() {
+        return ResponseEntity.ok(categoryService.getCurrentBalance());
+    }
+}
