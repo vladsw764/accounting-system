52c8cfcde26a8052839f6d495e3a639db824da92
Vladyslav <vladsw764@icloud.com>
2023-06-08 14:13:31 +0300

    added Date Time Formatter properties

--- src/main/java/com/example/accounting_system/dtos/DebtDto.java
+++ src/main/java/com/example/accounting_system/dtos/DebtDto.java
@@ -2,6 +2,7 @@ package com.example.accounting_system.dtos;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -9,7 +10,9 @@ import java.util.Date;
 @Getter
 @Setter
 public class DebtDto {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date endDate;
     private String category;
     private BigDecimal receivedAmount;
--- src/main/java/com/example/accounting_system/dtos/PaymentDto.java
+++ src/main/java/com/example/accounting_system/dtos/PaymentDto.java
@@ -2,6 +2,7 @@ package com.example.accounting_system.dtos;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -9,6 +10,7 @@ import java.util.Date;
 @Getter
 @Setter
 public class PaymentDto {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
     private BigDecimal amount;
 }
