41bf25e0071b97b709c5fa2d927f13b7b5d2bf3c
Vladyslav <vladsw764@icloud.com>
2023-06-13 22:44:05 +0300

    added configuration for JavaMailSender

--- src/main/java/com/example/accounting_system/config/AppConfig.java
+++ src/main/java/com/example/accounting_system/config/AppConfig.java
@@ -1,13 +1,44 @@
 package com.example.accounting_system.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
-public class AppConfig {
+import java.util.Properties;
 
+@Configuration
+@EnableScheduling
+public class AppConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private int port;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setPassword(password);
+        mailSender.setUsername(username);
+        Properties props = new Properties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        mailSender.setJavaMailProperties(props);
+
+        return mailSender;
+    }
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
