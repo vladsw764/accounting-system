8709b87c4c2aeb143a4e537573d3eab0192c6a9c
Vladyslav <vladsw764@icloud.com>
2023-06-13 21:22:27 +0300

    add CrossOrigin

--- src/main/java/com/example/accounting_system/controllers/CategoryController.java
+++ src/main/java/com/example/accounting_system/controllers/CategoryController.java
@@ -3,16 +3,14 @@ package com.example.accounting_system.controllers;
 import com.example.accounting_system.services.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
+@CrossOrigin("*")
 public class CategoryController {
     private final CategoryService categoryService;
 
--- src/main/java/com/example/accounting_system/controllers/TransactionController.java
+++ src/main/java/com/example/accounting_system/controllers/TransactionController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/transaction")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class TransactionController {
 
     private final TransactionService transactionService;
