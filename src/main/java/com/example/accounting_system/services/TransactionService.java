8b914a2648e606934732d9b552e7facf0d554d2d
Vladyslav <vladsw764@icloud.com>
2023-06-14 17:21:28 +0300

    Refactor TransactionService: Javadoc, Balance repository, balance statistics, daily balance task, code improvements.

--- src/main/java/com/example/accounting_system/services/TransactionService.java
+++ src/main/java/com/example/accounting_system/services/TransactionService.java
@@ -1,16 +1,20 @@
 package com.example.accounting_system.services;
 
 import com.example.accounting_system.dtos.TransactionDto;
+import com.example.accounting_system.entities.Balance;
 import com.example.accounting_system.entities.Transaction;
+import com.example.accounting_system.repositories.BalanceRepository;
 import com.example.accounting_system.repositories.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -18,10 +22,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TransactionService {
     private final TransactionRepository transactionRepository;
+    private final BalanceRepository balanceRepository;
     private final RestTemplate restTemplate;
     @Value("${server.url}")
     private String url;
 
+    /**
+     * Adds a new transaction.
+     *
+     * @param transactionDto the transaction DTO
+     * @return the created transaction
+     */
     public Transaction addTransaction(TransactionDto transactionDto) {
         Transaction transaction = convertToTransaction(transactionDto);
         log.info("check transaction amount");
@@ -35,6 +46,14 @@ public class TransactionService {
         return transactionRepository.save(transaction);
     }
 
+    /**
+     * Updates an existing transaction.
+     *
+     * @param transactionId  the ID of the transaction to update
+     * @param transactionDto the updated transaction DTO
+     * @return the updated transaction
+     * @throws RuntimeException if the transaction is not found
+     */
     public Transaction updateTransaction(Long transactionId, TransactionDto transactionDto) {
         Transaction existTransaction = transactionRepository.findById(transactionId)
                 .orElseThrow(() -> new RuntimeException("Transaction not found with ID: " + transactionId));
@@ -52,15 +71,32 @@ public class TransactionService {
         return transactionRepository.save(existTransaction);
     }
 
+    /**
+     * Retrieves a transaction by its ID.
+     *
+     * @param transactionId the ID of the transaction to retrieve
+     * @return the retrieved transaction
+     * @throws RuntimeException if the transaction is not found
+     */
     public Transaction getTransactionById(Long transactionId) {
         return transactionRepository.findById(transactionId)
                 .orElseThrow(() -> new RuntimeException("Transaction not found with ID: " + transactionId));
     }
 
+    /**
+     * Retrieves all transactions.
+     *
+     * @return a list of all transactions
+     */
     public List<Transaction> getAllTransactions() {
         return transactionRepository.findAll();
     }
 
+    /**
+     * Retrieves the current balance by summing all transaction amounts.
+     *
+     * @return the current balance
+     */
     public BigDecimal getCurrentBalance() {
         List<Transaction> transactions = transactionRepository.findAll();
         BigDecimal balance = BigDecimal.ZERO;
@@ -72,6 +108,12 @@ public class TransactionService {
         return balance;
     }
 
+    /**
+     * Removes a transaction from the transaction history.
+     *
+     * @param transactionId the ID of the transaction to remove
+     * @throws RuntimeException if the transaction is not found
+     */
     public void removeTransactionFromHistory(Long transactionId) {
         transactionRepository.findById(transactionId)
                 .orElseThrow(() -> new RuntimeException("Transaction not found with ID: " + transactionId));
@@ -79,6 +121,12 @@ public class TransactionService {
         transactionRepository.deleteById(transactionId);
     }
 
+    /**
+     * Converts a transaction DTO to a Transaction entity.
+     *
+     * @param transactionDto the transaction DTO to convert
+     * @return the converted Transaction entity
+     */
     private Transaction convertToTransaction(TransactionDto transactionDto) {
         Transaction transaction = new Transaction();
         transaction.setDate(transactionDto.getDate());
@@ -87,7 +135,12 @@ public class TransactionService {
         return transaction;
     }
 
-    // Get total balance from GetMapping(
+    /**
+     * Retrieves the total balance from an external API.
+     *
+     * @return the total balance
+     * @throws RuntimeException if the API call fails
+     */
     public BigDecimal getTotalBalance() {
         ResponseEntity<BigDecimal> response = restTemplate.getForEntity(url + "/api/v1/transaction/balance", BigDecimal.class);
         if (response.getStatusCode().is2xxSuccessful()) {
@@ -97,7 +150,46 @@ public class TransactionService {
         }
     }
 
-    // Check if transaction amount greater than total balance
+    /**
+     * Retrieves the balance statistics.
+     *
+     * @return a list of balance statistics
+     */
+    public List<Balance> getBalanceStatistics() {
+        return balanceRepository.findAll();
+    }
+
+    /**
+     * Scheduled task that collects and saves the daily balance.
+     */
+    @Scheduled(cron = "0 0 0 * * *") // Run daily at midnight
+    public void collectDailyBalance() {
+        BigDecimal currentBalance = getTotalBalance();
+        createBalance(currentBalance);
+    }
+
+    /**
+     * Creates a new balance entry with the specified amount.
+     *
+     * @param amount the amount to set for the balance
+     */
+    private void createBalance(BigDecimal amount) {
+        LocalDate currentDate = LocalDate.now();
+        Balance balance = new Balance();
+        balance.setDate(currentDate);
+        balance.setBalanceAmount(amount);
+
+        balanceRepository.save(balance);
+    }
+
+    /**
+     * Checks if the transaction amount is greater than the total balance.
+     * If the transaction amount is less than or equal to zero, it is multiplied by -1.
+     * Throws a RuntimeException if the transaction amount is greater than the total balance or the total balance is zero.
+     *
+     * @param transactionAmount The amount of the transaction to be checked
+     * @throws RuntimeException If the transaction amount is invalid
+     */
     public void checkTransactionAmount(BigDecimal transactionAmount) {
         BigDecimal totalBalance = getTotalBalance();
 
