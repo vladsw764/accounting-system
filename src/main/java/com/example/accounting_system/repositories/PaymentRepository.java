a547c647968fc9897ee01ad72eec1bf643696076
Vladyslav <vladsw764@icloud.com>
2023-06-15 23:16:17 +0300

    minor edits (add method 'findAllByDebtId')

--- src/main/java/com/example/accounting_system/repositories/PaymentRepository.java
+++ src/main/java/com/example/accounting_system/repositories/PaymentRepository.java
@@ -3,5 +3,8 @@ package com.example.accounting_system.repositories;
 import com.example.accounting_system.entities.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findAllByDebtId(Long id);
 }
